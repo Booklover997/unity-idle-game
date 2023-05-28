6256b4338176b562a0821dc9c023929958e0eb31
Booklover997 <65503002+Booklover997@users.noreply.github.com>
2023-05-27 21:40:47 -0400

    Finished the item class
    Fleshed out the rest of the item class allowing the slider to move and more of an item to be bought later iterations should add a way for the progress to not start unless the count is at least one.

--- idle test game/Assets/Item_Creator.cs
+++ idle test game/Assets/Item_Creator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class Item_Creator : MonoBehaviour
 {
     public GameObject Logic_Manager;
@@ -12,17 +13,27 @@ public class Item_Creator : MonoBehaviour
     public int Count;
     public TMP_Text countText;
     public int increaseAmount;
-    public int timeToProduce;
+    public float timeToProduce;
+    public Slider ProductionSlider;
+    private Logic_Script.item newItem; 
+    public float FillSpeed;
+    public Button BuyButton;
+    public int Price;
     // Start is called before the first frame update
     void Start()
     {
         logic = Logic_Manager.GetComponent<Logic_Script>();
-        Logic_Script.item newItem = new Logic_Script.item(Icon, nameText, Name, Count, countText);
+        newItem = new Logic_Script.item(Icon, nameText, Name, Count, countText, increaseAmount, timeToProduce, ProductionSlider, FillSpeed, BuyButton, Price);
+        System.Console.WriteLine();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        newItem.produce();
+    }
+    [ContextMenu("Increase Count")]
+    public void increaseCount(){
+        newItem.buy();
     }
 }
--- idle test game/Assets/Logic_Script.cs
+++ idle test game/Assets/Logic_Script.cs
@@ -5,31 +5,73 @@ using TMPro;
 using UnityEngine.UI;
 public class Logic_Script : MonoBehaviour
 {
-    public TMP_Text scoreText;
-    public int money;
+
     public class item {
         public TMP_Text nameText;
         public string Name;
         public SpriteRenderer Icon;
         public int Count;
         public TMP_Text countText;
-        public item(SpriteRenderer itemIcon, TMP_Text itemNameText, string itemName, int itemCount, TMP_Text itemCountText){
+        public int increaseAmount;
+        public float timeToProduce;
+        public Slider ProductionSlider;
+        public static int money = 0;
+        private float timer= 0;
+        public float TargetFill;
+        public float fillSpeed;
+        public Button BuyButton;
+        public int Price;
+        public static TMP_Text scoreText = GameObject.FindGameObjectWithTag("MoneyText").GetComponent<TextMeshProUGUI>();
+        public item(SpriteRenderer itemIcon, TMP_Text itemNameText, string itemName, int itemCount, TMP_Text itemCountText, int itemIncreaseAmount, float itemTimeToProduce, Slider itemProductionSlider, float itemFillSpeed, Button itemBuyButton, int itemPrice){
             nameText = itemNameText;
             Name = itemName;
             Icon = itemIcon;
             countText = itemCountText;
             Count = itemCount;
-            itemCountText.text = itemCount.ToString();
-            itemNameText.text = itemName.ToString();
-
+            countText.text = Count.ToString();
+            nameText.text = Name.ToString();
+            increaseAmount = itemIncreaseAmount;
+            timeToProduce = itemTimeToProduce;
+            ProductionSlider = itemProductionSlider;
+            TargetFill = 0;
+            fillSpeed = itemFillSpeed;
+            BuyButton = itemBuyButton;
+            Price = itemPrice;
+            BuyButton.onClick.AddListener(OnButtonClick);
+        }
+        public void OnButtonClick(){
+            buy();
+        }
+        public void produce(){
+            if (ProductionSlider.value < TargetFill){
+                ProductionSlider.value += fillSpeed * Time.deltaTime;
+            }
+            if (timer < timeToProduce){
+            TargetFill = timer/timeToProduce;
+            timer = timer + Time.deltaTime;
 
         }
-    }
-    [ContextMenu("Increase Score")]
-    public void addScore(){
-        money += 1;
-        scoreText.text = money.ToString();
+        else
+        {
+            item.money += Count * increaseAmount;
+            scoreText.text = item.money.ToString();
+            timer = 0;
+            ProductionSlider.value = 0;
+        }
 
+        }
+        public void buy(){
+            if (item.money >= Price){
+            Count +=1;
+            countText.text = Count.ToString();
+            item.money -= Price;
+            scoreText.text = item.money.ToString();
+            //TODO subtract from price
+            }
+            //TODO add error text 
+        }
     }
+
     
 }
+// I hate chinese people
